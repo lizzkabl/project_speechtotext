f0c88f2fff3cd10f74615c2e77f218faf33dfdad
lizzkabl <90633859+lizzkabl@users.noreply.github.com>
2021-11-12 13:10:09 +0300

    Add files via upload

--- bot2.2.py
+++ bot2.2.py
@@ -2,10 +2,19 @@ import requests
 import telebot
 import config
 import pytz
-import speech_recognition as sr
 import subprocess
 import random
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import io
+from google.cloud import speech_v1 as speech
+from google.oauth2 import service_account
+text=[]
+
+key_path = r"C:\Users\user\Desktop\bot\heroic-charter-331617-ee3ee22d50d3.json"
+credentials = service_account.Credentials.from_service_account_file(
+    key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
+)
+
 
 
 P_TIMEZONE = pytz.timezone(config.TIMEZONE)
@@ -24,12 +33,17 @@ def start(message):
                      InlineKeyboardButton('текст', callback_data='text'))
         question = 'что ты хочешь сделать?'
         bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+        bot.send_message(message.from_user.id, " функция 'текст в текст'- перевести Ваш текст в более сжатый вариант")
+        bot.send_message(message.from_user.id, "функция 'аудио в текст'- перевести Ваше голосовое сообщение в сжатый текстовый вариант ")
+        bot.send_message(message.from_user.id, "функция 'аудио в аудио'- перевести Ваше голосовое сообщение в более сжатое аудиосообщение ")
+        bot.send_message(message.from_user.id,
+                         "функция 'текст'- вывести текст из голосового сообщения")
     else:
         bot.send_message(message.from_user.id, 'Напиши /start')
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     if call.data == "text":
-        bot.answer_callback_query(call.id, "пришлите код")
+        bot.answer_callback_query(call.id, "пришлите голосовое сообщение")
         @bot.message_handler(content_types=['voice'])
         def handle_audio(message):
             global text
@@ -49,10 +63,22 @@ def callback_query(call):
 
             process = subprocess.run(['ffmpeg', '-i', src_filename, dest_filename])
 
-            r = sr.Recognizer()
+            '''r = sr.Recognizer()
             with sr.AudioFile(dest_filename) as source:
                 audio = r.record(source)
-                text = r.recognize_google(audio, language="ru-RU")
+                text = r.recognize_google(audio, language="ru-RU")'''
+            client = speech.SpeechClient(credentials=credentials)
+            with io.open(dest_filename, "rb") as audio_file:
+                content = audio_file.read()
+            audio = speech.RecognitionAudio(content=content)
+            config = speech.RecognitionConfig(
+                encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+                language_code="ru_Ru",
+            )
+            response = client.recognize(config=config, audio=audio)
+
+            for result in response.results:
+                text.append(result.alternatives[0].transcript)
             bot.send_message(message.from_user.id, text)
     elif call.data == "texttotext":
         bot.answer_callback_query(call.id, "к сожалению данная кнопка пока в разработке")
@@ -60,8 +86,5 @@ def callback_query(call):
         bot.answer_callback_query(call.id, "к сожалению данная кнопка пока в разработке")
     elif call.data == "audiotoaudio":
         bot.answer_callback_query(call.id, "к сожалению данная кнопка пока в разработке")
-
-
-
 
 bot.polling(none_stop=True, interval=0)
